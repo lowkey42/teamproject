07de9c0cf87405d9805052ccfc3dbc9b99535b8d
lowkey <mephistopheles@onlinehome.de>
2015-07-31 01:02:56 +0200

    highscore; savegames and some critical bug-fixes -- http://asset-3.soupcdn.com/asset/12056/2589_30fa.gif

---
+++ src/game/highscore.hpp
@@ -0,0 +1,36 @@
+/**************************************************************************\
+ * Manages the highscore list (load, store, add)                          *
+ *                                               ___                      *
+ *    /\/\   __ _  __ _ _ __  _   _ _ __ ___     /___\_ __  _   _ ___     *
+ *   /    \ / _` |/ _` | '_ \| | | | '_ ` _ \   //  // '_ \| | | / __|    *
+ *  / /\/\ \ (_| | (_| | | | | |_| | | | | | | / \_//| |_) | |_| \__ \    *
+ *  \/    \/\__,_|\__, |_| |_|\__,_|_| |_| |_| \___/ | .__/ \__,_|___/    *
+ *                |___/                              |_|                  *
+ *                                                                        *
+ * Copyright (c) 2014 Florian Oetke                                       *
+ *                                                                        *
+ *  This file is part of MagnumOpus and distributed under the MIT License *
+ *  See LICENSE file for details.                                         *
+\**************************************************************************/
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace mo {
+	namespace asset{class Asset_manager;}
+
+
+	struct Score {
+		std::string name;
+		int32_t     score;
+		int32_t     level;
+		uint64_t    seed;
+	};
+
+	extern void add_score(asset::Asset_manager& assets, Score score);
+
+	extern auto list_scores(asset::Asset_manager& assets) -> std::vector<Score>;
+
+}
